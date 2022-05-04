d678ef4d81575e6aae1b8388e828d6a41025715c
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-05-04 14:46:05 -0400

    day 1

---
+++ components/sidebar.tsx
@@ -0,0 +1,34 @@
+import {
+  Box,
+  List,
+  ListIcon,
+  Divider,
+  ListItem,
+  Center,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/layout";
+
+import {
+  MdHome,
+  MdSearch,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdFavorite,
+} from "react-icons/md";
+
+const Sidebar = () => {
+  return (
+    <Box
+      width="100%"
+      height="calc(100vh - 100px)"
+      bg="black"
+      paddingX="5px"
+      color="gray"
+    >
+      js
+    </Box>
+  );
+};
+
+export default Sidebar;
